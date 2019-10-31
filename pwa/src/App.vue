6fb19fc1068b02b3ff3444e8ddc9e0463efa1009
Julian Beck <ju-fa-beck@t-online.de>
2019-10-31 11:19:02 +0100

    added router and icons

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>BEJU</span>
-        <span class="font-weight-light">AS A SERVICE</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">LAI</span>
-      </v-btn>
-    </v-app-bar>
+  <div id="app">
+    <v-app>
+      <v-app-bar app>
+        <v-toolbar-title class="headline text-uppercase">
+          <span>BEJU</span>
+          <span class="font-weight-light">AS A SERVICE</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+          <span class="mr-2">LAI</span>
+        </v-btn>
+      </v-app-bar>
 
-    <v-content>
-      <StartPage />
-    </v-content>
-  </v-app>
+      <v-content>
+        <router-view />
+      </v-content>
+    </v-app>
+  </div>
 </template>
 
-<script>
-import StartPage from "./components/StartPage";
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: {
-    StartPage
-  },
-  data: () => ({
-    //
-  })
-};
-</script>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ pwa/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <StartPage />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import StartPage from '@/components/StartPage.vue'
+
+export default {
+  name: 'home',
+  components: {
+    StartPage
+  }
+}
+</script>
