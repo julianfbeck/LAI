30e508e048f3aa573bc6432e1c44a94e193d9c90
Henning Häcker <henning.haecker+github.com@gmail.com>
2019-11-02 15:01:29 +0100

    WIP: add ilias test instance on 8080

--- terraform/vpc/secgroups.tf
+++ terraform/vpc/secgroups.tf
@@ -23,6 +23,16 @@ resource "openstack_networking_secgroup_rule_v2" "terraform-http" {
   security_group_id = "${openstack_networking_secgroup_v2.my-secgroup.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "terraform-http-proxy" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 8080
+  port_range_max    = 8080
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.my-secgroup.id}"
+}
+
 resource "openstack_networking_secgroup_rule_v2" "terraform-https" {
   direction         = "ingress"
   ethertype         = "IPv4"
