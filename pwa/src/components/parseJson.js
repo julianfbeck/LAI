4a5df1746fcf2cedd53aac9e81c5985bcfa3b08f
Julian Beck <ju-fa-beck@t-online.de>
2019-11-05 15:32:55 +0100

    process basic information

--- pwa/src/components/parseJson.js
+++ pwa/src/components/parseJson.js
@@ -4,9 +4,15 @@ const getUsers = (json) => {
 }
 
 const getOverview = (json) => {
-    return json.filter(sheet => sheet.sheetName === "Testergebnisse")
+    //get Testergebnisse sheet
+    const data = json.filter(sheet => sheet.sheetName === "Testergebnisse")[0].data
+    //remove last collumn
+    const overview = data.filter(sheet => sheet["Benutzername"] !== undefined)
+    const maxPoints = overview.reduce((a,b) => a + Number(b["Maximal erreichbare Punktezahl"]), 0)
+    const totalPoints = overview.reduce((a,b) => a + Number(b["Testergebnis in Punkten"]), 0)
+    const totalTestRuns  =  overview.reduce((a,b) => a + Number(b["Durchlauf"]), 0)
+    return {maxPoints: maxPoints, totalPoints: totalPoints, totalTestRuns: totalTestRuns}
 }
-
 const parse = {
     getOverview,
     getUsers,
