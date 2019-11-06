d118a4ea2a1feee052b208f20de3e3f75438c842
Julian Beck <ju-fa-beck@t-online.de>
2019-11-06 14:00:44 +0100

    added tabs

--- pwa/src/views/Home.vue
+++ pwa/src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="home">
-    <StartPage />
-  </div>
+  <v-tabs >
+    <v-tab ripple>Overview</v-tab>
+    <v-tab ripple>Questions</v-tab>
+    <v-tab ripple>Diagrams</v-tab>
+    <v-tab-item>
+      <v-card flat>
+        <StartPage />
+      </v-card>
+    </v-tab-item>
+    <v-tab-item>
+      <v-card flat>
+        <v-card-text>Contents for Item 2 go here</v-card-text>
+      </v-card>
+    </v-tab-item>
+  </v-tabs>
 </template>
 
 <script>
 // @ is an alias to /src
-import StartPage from '@/components/StartPage.vue'
+import StartPage from "@/components/StartPage.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     StartPage
   }
-}
+};
 </script>
