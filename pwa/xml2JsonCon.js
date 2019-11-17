a5f87c7e726183ecbd4c9fbc9e98f76d7414768b
Julian Beck <ju-fa-beck@t-online.de>
2019-11-17 15:24:18 +0100

    added parsed json

---
+++ pwa/xml2JsonCon.js
@@ -0,0 +1,10 @@
+var fs = require('fs'),
+    xml2js = require('xml2js');
+ 
+var parser = new xml2js.Parser();
+fs.readFile("/Users/julianbeck/Documents/Programming/node/LAI/samples/Commented_XML.xml", function(err, data) {
+    parser.parseString(data, function (err, result) {
+        console.log(result)
+        fs.writeFileSync('./result.json', JSON.stringify(result) , 'utf-8'); 
+    });
+});ci 
