9e738f0bf02b51b63a51234d84e45138d3f5db66
SiggiSigmann <siggisigmann@googlemail.com>
2019-11-20 09:24:57 +0100

    Update Privacy.vue

--- pwa/src/views/Privacy.vue
+++ pwa/src/views/Privacy.vue
@@ -1,5 +1,5 @@
 <template>
   <v-container>
-    <h1>Für was brauchen sie hilfe? sie sind schließlich hier der Professor. Mehr Arbeiten weniger drucken</h1>
+    <h1>coming soon</h1>
   </v-container>
 </template>
